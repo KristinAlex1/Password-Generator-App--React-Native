59e44a0fae6d8e0f493f4b9d442ce77299b1bcae
KristinAlex1 <kristinalex2001@gmail.com>
2025-01-12 02:12:39 -0400

    Added basic UI design

---
+++ AppPro.tsx
@@ -0,0 +1,138 @@
+import { Image, SafeAreaView, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native'
+import React from 'react'
+import App from './App'
+import BouncyCheckbox from 'react-native-bouncy-checkbox';
+
+export default function AppPro() {
+  return (
+    <ScrollView>
+        <SafeAreaView style = {styles.Appcontainer}>
+            <View style={styles.container}>
+                <Image source={{uri:'https://cdn-icons-png.flaticon.com/128/891/891399.png'}} style={styles.headingImage}/>
+                <Text style={styles.headingText}>Lock-Key</Text>
+            </View>
+            <View style={styles.formContainer}>
+                <View style = {styles.lengthContainer}>
+                <Text style = {styles.formHeading}>Password Length</Text>
+                <TextInput style={styles.textInput}></TextInput>
+                </View>
+                <View style = {styles.lowerCaseContainer}>
+                <Text style = {styles.CheckBoxHeading}>Include Lowercase</Text>
+                <BouncyCheckbox 
+                disableBuiltInState 
+                fillColor="#29ab87"
+                style={styles.LowercaseCheckbox}/>
+                </View>
+                <View style = {styles.lowerCaseContainer}>
+                <Text style = {styles.CheckBoxHeading}>Include Uppercase</Text>
+                <BouncyCheckbox 
+                disableBuiltInState 
+                fillColor="yellow"
+                style={styles.LowercaseCheckbox}/>
+                </View>
+                <View style = {styles.lowerCaseContainer}>
+                <Text style = {styles.CheckBoxHeading}>Include Numbers  </Text>
+                <BouncyCheckbox 
+                disableBuiltInState 
+                fillColor="pink"
+                style={styles.LowercaseCheckbox}/>
+                </View>
+                <View style = {styles.lowerCaseContainer}>
+                <Text style = {styles.CheckBoxHeading}>Include Symbols  </Text>
+                <BouncyCheckbox 
+                disableBuiltInState 
+                fillColor="red"
+                style={styles.LowercaseCheckbox}/>
+                </View>
+
+
+            </View>
+            
+        </SafeAreaView>
+    </ScrollView>
+  )
+}
+
+const styles = StyleSheet.create({
+    Appcontainer:{
+        backgroundColor:'#011638',
+        flex: 1,
+       
+    },
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginLeft: 20,
+        
+
+        
+        
+
+    },
+    headingImage:{
+        width: 50,
+        height: 50,
+        marginLeft: 10,
+    },
+    headingText:{
+        paddingVertical:5,
+        fontSize: 50,
+        fontWeight: 'bold',
+        fontFamily: 'monospace',
+
+
+    },
+    formContainer: {
+        
+        borderRadius: 10,
+        backgroundColor: '#141414',
+        padding: 20,
+        margin: 20,
+        borderBlockColor: '#EEC643',
+        borderWidth: 10,
+        
+        
+
+    },
+    formHeading:{
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#EEF0F2',
+        fontFamily: 'monospace',
+
+    },
+    lengthContainer:{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginVertical: 25,
+        alignItems: 'center',
+        margin: 10,
+
+    },
+    textInput:{
+        height: 30,
+        width: 100,
+        backgroundColor: 'gray',
+        borderRadius: 5,
+        
+
+    },
+    lowerCaseContainer:{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginVertical: 25,
+        margin: 10,
+    },
+    CheckBoxHeading:{
+        fontSize: 19,
+        fontWeight: 'bold',
+        color: '#EEF0F2',
+        fontFamily: 'monospace',
+    },
+    LowercaseCheckbox:{
+        marginLeft: 45,
+    }
+        
+})
